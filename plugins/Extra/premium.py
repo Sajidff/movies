579be4ff15953befb263419ca660ee3f0e8e2858
Sajidff <tyryr1243@gmail.com>
2025-06-22 13:24:11 +0530

    Update premium.py

--- plugins/Extra/premium.py
+++ plugins/Extra/premium.py
@@ -159,7 +159,7 @@ async def plan(client, message):
     users = message.from_user.mention 
     btn = [[
 	
-        InlineKeyboardButton("⌯ ᴄʜᴇᴄᴋ ᴀʟʟ ᴘʟᴀɴs ⌯", callback_data='free')],[InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ ❌", callback_data="close_data")
+        InlineKeyboardButton("⌯ ᴄʜᴇᴄᴋ ᴀʟʟ ᴘʟᴀɴs ⌯", callback_data='free')],[InlineKeyboardButton("⌯ ᴄʟᴏꜱᴇ ⌯", callback_data="close_data")
     ]]
     await message.reply_photo(photo="https://graph.org/file/55a5392f88ec5a4bd3379.jpg", caption=script.PREPLANS_TXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
     
