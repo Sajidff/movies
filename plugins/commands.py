124bab9ee05d02eceb37d16a2fb59dfa429ac2c1
Sajidff <tyryr1243@gmail.com>
2025-06-22 12:41:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -966,7 +966,7 @@ async def refer(bot, message):
     btn = [[
         InlineKeyboardButton('invite link', url=f'https://telegram.me/share/url?url=https://t.me/{bot.me.username}?start=reff_{message.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83'),
         InlineKeyboardButton(f'⏳ {referdb.get_refer_points(message.from_user.id)}', callback_data='ref_point'),
-        InlineKeyboardButton('⌯ ᴄʜᴏsᴇ ⌯, callback_data='close_data')
+        InlineKeyboardButton('⌯ ᴄʜᴏsᴇ ⌯', callback_data='close_data')
     ]]  
     m=await message.reply_sticker("CAACAgUAAxkBAAEXCjloV4WaZtRhNIiUf8i6XAlIswFT8QACyRIAAsX1WVb9jT\_W5YBrdTYE")      
     await m.delete()
